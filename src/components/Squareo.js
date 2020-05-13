fcc6062e3608b687cc814fd0814951713e4a90cd
Miko Ademagic <miko.ademagic@gmail.com>
2020-05-13 16:39:07 +1000

    Removing state from Squareo

--- src/components/Squareo.js
+++ src/components/Squareo.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import './squareo.scss';
 
-class Squareo extends React.Component {
-    constructor(props) {
-        super(props);
+const STATUS_MESSAGES = [
+    {
+        valueFrom: 75,
+        message: 'I\'m peckish'
+    },
+    {
+        valueFrom: 50,
+        message: 'I\'m getting pretty hungry'
+    },
+    {
+        valueFrom: 25,
+        message: 'I\'m starving, feed me'
+    }
+];
 
-        this.state = {
-            status: 'hello yo soy squareo'
-        };
+function Squareo(props) {
+    let statusMessage = 'Yo Soy Squareo';
+
+    for (let i = 0; i < STATUS_MESSAGES.length; i++) {
+        if (props.hungerLevel < STATUS_MESSAGES[i].valueFrom) {
+            statusMessage = STATUS_MESSAGES[i].message;
+        }
     }
 
-    render() {
-        return (
+    return (
         <div className="squareo">
-            {this.props.hungerLevel}
+            {statusMessage}
         </div>
-        )
-    }
+    )
 }
 
 export default Squareo;
